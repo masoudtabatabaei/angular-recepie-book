f9d5b7e7be45ee567f457251a8c6d053fd027215
Masoud <masoudtabatabaei20@gmail.com>
2022-03-03 17:18:35 +0330

    Implement recipe item

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input } from '@angular/core';
+import {RecipeModel} from "../../recipe.model";
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input()
+  recipe:RecipeModel = new RecipeModel("","","");
 
   constructor() { }
 
