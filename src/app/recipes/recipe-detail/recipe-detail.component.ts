03fdb4806e6fde969e1e5ff03cd8a6fe49038180
Masoud <masoudtabatabaei20@gmail.com>
2022-03-03 18:24:27 +0330

    Implement mock Recipe Details section

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-
+  show = false;
   constructor() { }
 
   ngOnInit(): void {
